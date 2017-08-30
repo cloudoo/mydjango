74a4eddc8d2143e37698e182711e8283136af45e
cloudoo <leisongyun@gmail.com>
2017-08-30 22:09:24 +0800

    1.some formatter edit

--- polls/admin.py
+++ polls/admin.py
@@ -8,13 +8,14 @@ class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,              {'fields': ['question_text']}),
-        ('Date information',{'fields': ['pub_date'], 'classes': ['collapse']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
-    list_display = ('question_text','pub_date','was_published_recently')
-    inlines =  [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
     list_filter = ['pub_date']
     search_fields = ['question_text']
 
